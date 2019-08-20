dbabb3baa0982721807205ecd88bce85e4ca0141
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-20 11:14:15 -0400

    created server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3003;
+
+app.get('/', (req, res) => {
+  res.send(`Attention humans, we are observing you on PORT ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Attention humans, we are observing you on PORT ${PORT}`);
+});

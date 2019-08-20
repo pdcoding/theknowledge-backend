45d002038df117e0e7f07e115d67838a42e4e33e
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-20 15:00:34 -0400

    added middleware mogoose connection and user and session controllers

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const app = express();
 const PORT = 3003;
-const cors = require('cors');
+const MONGODB_URI = 'mongodb://localhost:27017';
 
 const whitelist = ['http://localhost:3000'];
 const corsOptions = {
@@ -15,7 +17,36 @@ const corsOptions = {
   }
 };
 
+//middleware
+app.use(
+  session({ secret: 'pineapple', resave: false, saveUninitialized: false })
+);
+app.use(express.json());
 app.use(cors(corsOptions));
+
+// Mongoose connection
+mongoose.connection.on('error', err => {
+  console.log(err.message + ' is Mongod not running?');
+});
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+// Fix depreciation warnings
+mongoose.set('useFindAndModify', false);
+
+//DATABASE CONNECTION
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+  console.log('Connected to mongoose...');
+});
+
+//User Controllers & routes
+const userController = require('./controllers/users.js');
+app.use('/users', userController);
+
+//Sessions controller
+const sessionsController = require('./controllers/sessions.js');
+app.use('/sessions', sessionsController);
 
 app.get('/', (req, res) => {
   res.send(`Attention humans, we are observing you on PORT ${PORT}`);

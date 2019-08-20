37d94a6a51de4b3306eaef25cca176f8aacf374a
davidlinke <davidlinke@davidlinke.com>
2019-08-20 16:18:36 -0400

    fixed sessions post route

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,26 +6,24 @@ const User = require('../models/users.js');
 
 //index
 users.get('/', (req, res) => {
-    User.find({}, (err, allUsers) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-        }
-        res.status(200).send(allUsers);
-      });
-    });
+	User.find({}, (err, allUsers) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+		}
+		res.status(200).send(allUsers);
+	});
+});
 
 //Create
 users.post('/', (req, res) => {
-	User.findOne({ name: req.body.username }, (err, foundUser) => {
+	User.findOne({ email: req.body.email }, (err, foundUser) => {
 		if (bcrypt.compareSync(req.body.password, foundUser.password)) {
 			req.session.currentUser = foundUser;
-			res.redirect('/');
+			res.send('login correct');
 		} else {
 			res.send('wrong password');
 		}
 	});
 });
 
-
 module.exports = users;
-

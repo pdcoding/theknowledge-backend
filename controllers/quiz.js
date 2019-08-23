d49b55187966a6a598d918dce2ad6f5b8e46da04
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-23 10:31:04 -0400

    push to pull

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -5,60 +5,60 @@ const Quiz = require('../models/quiz');
 
 //index (quizzes list)
 quizzes.get('/', (req, res) => {
-	Quiz.find({}, (err, allQuizzes) => {
-		let quizArray = [];
-		for (i = 0; i < allQuizzes.length; i++) {
-			let quizObject = {
-				name: allQuizzes[i].name,
-				caption: allQuizzes[i].caption,
-				image: allQuizzes[i].image,
-				createdBy: allQuizzes[i].createdBy,
-				createdAt: allQuizzes[i].created_at
-			};
-			quizArray.push(quizObject);
-		}
-		if (err) {
-			res.status(400).json({ error: err.message });
-		}
-		res.status(200).send(quizArray);
-	});
+  Quiz.find({}, (err, allQuizzes) => {
+    let quizArray = [];
+    for (i = 0; i < allQuizzes.length; i++) {
+      let quizObject = {
+        name: allQuizzes[i].name,
+        caption: allQuizzes[i].caption,
+        image: allQuizzes[i].image,
+        createdBy: allQuizzes[i].createdBy,
+        createdAt: allQuizzes[i].created_at
+      };
+      quizArray.push(quizObject);
+    }
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.status(200).send(quizArray);
+  });
 });
 
 // create
 quizzes.post('/', (req, res) => {
-	const cookies = parseCookies(req.headers.cookie);
-	const userObjectID = cookies.userid;
-	const updatedQuiz = req.body;
-	Object.assign(updatedQuiz, { createdBy: userObjectID });
-	console.log(updatedQuiz);
+  const cookies = parseCookies(req.headers.cookie);
+  const userObjectID = cookies.userid;
+  const updatedQuiz = req.body;
+  Object.assign(updatedQuiz, { createdBy: userObjectID });
+  console.log(updatedQuiz);
 
-	Quiz.create(updatedQuiz, (err, createdQuiz) => {
-		if (err) {
-			res.status(400).json({ error: err.message });
-		} else {
-			res.status(200).send(createdQuiz);
-		}
-	});
+  Quiz.create(updatedQuiz, (err, createdQuiz) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.status(200).send(createdQuiz);
+    }
+  });
 });
 
 quizzes.get('/', (req, res) => {
-	//auth logic here
-	// do an axios call to this route
-	console.log(req.headers.cookie);
-	console.log(req.session);
-	console.log('test');
+  //auth logic here
+  // do an axios call to this route
+  console.log(req.headers.cookie);
+  console.log(req.session);
+  console.log('test');
 
-	res.send('test');
+  res.send('test');
 });
 
 // delete
 quizzes.delete('/:id', (req, res) => {
-	Quiz.findByIdAndRemove(req.params.id, (err, deletedQuiz) => {
-		if (err) {
-			res.status(400).json({ error: err.message });
-		}
-		res.status(200).json(deletedQuiz);
-	});
+  Quiz.findByIdAndRemove(req.params.id, (err, deletedQuiz) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.status(200).json(deletedQuiz);
+  });
 });
 
 //MAKE a route that pulls all quiz information from a specific quiz
@@ -68,13 +68,13 @@ quizzes.delete('/:id', (req, res) => {
 
 //Quiz info pull
 quizzes.get('/:id', (req, res) => {
-	Quiz.findById(req.params.id, (err, quizInfo) => {
-		if (err) {
-			res.status(400).json({ error: err.message });
-		} else {
-			res.json(quizInfo);
-		}
-	});
+  Quiz.findById(req.params.id, (err, quizInfo) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.json(quizInfo);
+    }
+  });
 });
 
 //seed
@@ -91,12 +91,12 @@ module.exports = quizzes;
 
 // Adapted from https://stackoverflow.com/questions/3393854/
 const parseCookies = cookies => {
-	let cookiesObj = {};
+  let cookiesObj = {};
 
-	cookies.split(';').forEach(cookie => {
-		let parts = cookie.split('=');
-		cookiesObj[parts.shift().trim()] = decodeURI(parts.join('='));
-	});
+  cookies.split(';').forEach(cookie => {
+    let parts = cookie.split('=');
+    cookiesObj[parts.shift().trim()] = decodeURI(parts.join('='));
+  });
 
-	return cookiesObj;
+  return cookiesObj;
 };

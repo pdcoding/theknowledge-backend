f23d5ca07c641414497ac26a67f9d1d34cf3b2f9
davidlinke <davidlinke@davidlinke.com>
2019-08-23 09:36:57 -0400

    fixed issues with user not being added to the quiz they create and created get display name route

--- models/quiz.js
+++ models/quiz.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const quizSchema = new Schema({
-  name: String,
-  caption: String,
-  image: String,
-  createdBy: String,
-  questions: [Object],
-  results: [Object]
-},{
-  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-}
+const quizSchema = new Schema(
+	{
+		name: String,
+		caption: String,
+		image: String,
+		createdBy: String,
+		questions: [Object],
+		results: [Object]
+	},
+	{
+		timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+	}
 );
 
 const Quiz = mongoose.model('Quiz', quizSchema);

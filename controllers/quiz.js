c5f0ba2187f71f3c11f76505356d2937a7ce0690
davidlinke <davidlinke@davidlinke.com>
2019-08-23 14:14:44 -0400

    seed route tweaks

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -80,12 +80,14 @@ quizzes.get('/:id', (req, res) => {
 
 //seed
 // quizzes.get('/seed', (req, res) => {
-//   Quiz.create(seedData, (err, createdQuizzes) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//     } else console.log('Successfully seeded data');
-//     res.send('Data successfully seeded');
-//   });
+// 	Quiz.create(seedData, (err, createdQuizzes) => {
+// 		if (err) {
+// 			res.status(400).json({ error: err.message });
+// 		} else {
+// 			console.log('Successfully seeded data');
+// 			res.send('Data successfully seeded');
+// 		}
+// 	});
 // });
 
 module.exports = quizzes;
